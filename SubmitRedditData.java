e0e6f0f01e7aa88c72babaa5ec5a1b951bcca6b6
ButtCoin <53128734+butttcoin@users.noreply.github.com>
2019-08-01 15:57:47 -0400

    Create SubmitRedditData.java

---
+++ SubmitRedditData.java
@@ -0,0 +1,32 @@
+package RedditParser;
+
+public class SubmitRedditData {
+
+    private String username;
+    private Integer score;
+    private String url;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
